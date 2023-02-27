4ef976567adccf7d303b026fe19ac3a90d49bc39
Kshitiz <kshitizagarwa2405@gmail.com>
2023-02-27 14:45:58 +0530

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,7 +17,9 @@ void main() async {
   await settingsController.loadSettings();
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(
+      settingsController: settingsController,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
