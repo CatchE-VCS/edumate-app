b823121c02dda3acab7071a4c19344e7d95d13ef
Kshitiz <kshitizagarwa2405@gmail.com>
2023-02-27 14:43:05 +0530

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,19 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:edumate/main.dart';
 import 'package:edumate/src/settings/settings_controller.dart';
 import 'package:edumate/src/settings/settings_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() async {
   final settingsController = SettingsController(SettingsService());
 
   await settingsController.loadSettings();
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(settingsController: settingsController,));
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
