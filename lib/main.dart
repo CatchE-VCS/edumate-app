31c1d84e3d160e9a9832bfbe605f690289b4e638
Kshitiz <kshitizagarwa2405@gmail.com>
2023-02-27 14:39:18 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:edumate/src/settings/settings_controller.dart';
-import 'package:edumate/src/settings/settings_service.dart';
 
-void main() async {
-  final settingsController = SettingsController(SettingsService());
-  await settingsController.loadSettings();
-  runApp(MyApp(
-    settingsController: settingsController,
-  ));
-}
+import 'pages/home_page.dart';
+import 'themes/themes.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.settingsController});
-  final SettingsController settingsController;
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData.dark(),
-      //themeMode: settingsController.themeMode,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      theme: AppTheme.lightTheme,
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
