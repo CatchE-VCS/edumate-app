c1c93fd933cd0d35207e76b6647914b811ec0c6d
AKGEC <utkarsh2110024@akgec.ac.in>
2023-02-27 14:48:22 +0530

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'package:edumate/src/settings/settings_controller.dart';
+import 'package:edumate/src/settings/settings_service.dart';
 import 'pages/home_page.dart';
 import 'themes/themes.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  final settingsController = SettingsController(SettingsService());
+  await settingsController.loadSettings();
+  runApp(MyApp(
+    settingsController: settingsController,
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.settingsController});
+  final SettingsController settingsController;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      darkTheme: ThemeData.dark(),
       theme: AppTheme.lightTheme,
       home: HomePage(),
       debugShowCheckedModeBanner: false,
