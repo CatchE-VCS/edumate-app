0655f63e696193e7859feab6da72c1b7f94c9af4
Kshitiz <kshitizagarwa2405@gmail.com>
2023-03-03 13:06:23 +0530

    changes

--- lib/pages/yourclasses/courseProgress.dart
+++ lib/pages/yourclasses/courseProgress.dart
@@ -23,18 +23,22 @@ class CourseProgress extends StatelessWidget {
                 Row(
                   children: [
                     SizedBox(
-                      width: 15,
+                      width: 5,
                     ),
                     Image.network(
                       'https://cdn3.iconfinder.com/data/icons/faticons/32/grid-2-01-512.png',
-                      width: 25,
+                      width: 20,
                     ),
                     SizedBox(
-                      width: 15,
+                      width: 3,
                     ),
-                    Image.network(
-                      'https://www.freeiconspng.com/thumbs/list-icon/checklist-icon-checklist-icon-png-list-icon-7.png',
-                      width: 25,
+                    Container(
+                      width: 185,
+                      height: 25,
+                      child: Image.asset(
+                        'assets/images/list.png',
+                        color: Colors.white,
+                      ),
                     ),
                   ],
                 ),
@@ -119,19 +123,19 @@ class CourseProgress2 extends StatelessWidget {
                 Row(
                   children: [
                     SizedBox(
-                      width: 15,
+                      width: 5,
                     ),
                     Image.network(
                       'https://cdn3.iconfinder.com/data/icons/faticons/32/grid-2-01-512.png',
-                      width: 25,
+                      width: 20,
                     ),
                     SizedBox(
-                      width: 15,
+                      width: 3,
                     ),
-                    Image.network(
-                      'https://www.freeiconspng.com/thumbs/list-icon/checklist-icon-checklist-icon-png-list-icon-7.png',
-                      width: 25,
-                    ),
+                    Container(
+                        width: 185,
+                        height: 25,
+                        child: Image.asset('assets/images/list.png'))
                   ],
                 ),
               ],
